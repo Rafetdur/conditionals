8935f929d7870469819897c7bf4a2be6e8b4dfad
rafet <83957086+Rafetdur@users.noreply.github.com>
2022-01-29 13:07:09 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+
+        int sayi = 24;
+
+        if (sayi < 20) {
+            System.out.println("Sayi 20'den küçüktür");
+        }
+        else if (sayi ==20){
+            System.out.println("Sayi 20'ye eşittir");
+        }
+        else {
+            System.out.println("Sayi 20'den büyüktür");
+        }
+    }
+}
